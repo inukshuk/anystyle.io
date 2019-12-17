b64f3372ad6ea4e71d4fb3437b84bed2e67667c5
Sylvester Keil <sylvester@keil.or.at>
2019-12-17 22:05:33 +0100

    Use and initialize local model

--- config/initializers/anystyle.rb
+++ config/initializers/anystyle.rb
@@ -6,3 +6,16 @@ dictionary.freeze
 dictionary.db.freeze
 
 AnyStyle::Parser.defaults[:dictionary] = dictionary
+
+original_model = AnyStyle::Parser.defaults[:model]
+
+require 'fileutils'
+require 'pathname'
+
+pn = Pathname.new Rails.configuration.anystyle.model
+pn.parent.mkpath unless pn.parent.exist?
+
+FileUtils.cp original_model, pn.to_s unless pn.exist?
+
+AnyStyle::Parser.defaults[:model] =
+  Rails.configuration.anystyle.model.dup.untaint
