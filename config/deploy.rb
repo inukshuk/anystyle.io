d93e8bc90382c77eb2ab174295ff8882614ef42f
Sylvester Keil <sylvester@keil.or.at>
2024-01-11 11:10:31 +0100

    Update capistrano and ruby versions

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '3.14.1'
+lock '3.18.0'
 
 set :application, 'anystyle'
 set :repo_url, 'https://github.com/inukshuk/anystyle.io.git'
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/inukshuk/anystyle.io.git'
 set :deploy_to, '/var/www/anystyle'
 
 set :rails_env, 'production'
-set :chruby_ruby, 'ruby-2.6.4'
+set :chruby_ruby, 'ruby-3.2.2'
 
 append :linked_files,
 #  'config/database.yml',
